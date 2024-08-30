94f16b2e135d7ee42df46ad06f60517d008500c0
nomankhan33 <mohdnomankhan01@gmail.com>
2024-08-30 09:12:14 +0530

    Your commit message here

--- src/components/portfolio/Data.jsx
+++ src/components/portfolio/Data.jsx
@@ -1,42 +1,41 @@
-import Work1 from '../../assets/work1.jpg';
+import work1 from '../../assets/work1.jpg';
+import dog from '../../assets/dog.png';
+import stats from '../../assets/stats.jpg';
+import dozer from '../../assets/dozer.png'
 
 export const projectData = [
     {
         id: 1,
-        image: Work1,
-        title: 'Web Design',
-        category: 'AI/ML'
+        image: work1,
+        title: 'Portfolio',
+        category: 'Web',
+        link: 'https://mohd-noman-khan.netlify.app/',
+        git: 'https://github.com/nomankhan33/portfolio/'
     },
     {
         id: 2,
-        image: Work1,
-        title: 'Web Design',
-        category: 'AI/ML'
+        image: stats,
+        title: 'WhatsStats',
+        category: 'AI/ML',
+        link: 'https://whatsapp-chat-analyser-noman.streamlit.app/',
+        git: 'https://github.com/nomankhan33/WhatsApp-Chat-Analyser/'
     },
     {
         id: 3,
-        image: Work1,
-        title: 'Web Design',
-        category: 'Web'
+        image: dog,
+        title: 'PawIdentify',
+        category: 'AI/ML',
+        link: 'https://dog-breed-identify-noman.streamlit.app/',
+        git: 'https://github.com/nomankhan33/Dog-Breed-Identification/'
     },
     {
         id: 4,
-        image: Work1,
-        title: 'Web Design',
-        category: 'AI/ML'
+        image: dozer,
+        title: 'DozerVal',
+        category: 'AI/ML',
+        link: '',
+        git: 'https://github.com/nomankhan33/Bulldozer-Price-Prediction/'
     },
-    {
-        id: 5,
-        image: Work1,
-        title: 'Web Design',
-        category: 'AI/ML'
-    },
-    {
-        id: 6,
-        image: Work1,
-        title: 'Web Design',
-        category: 'Web'
-    }
 ]
 
 export const projectNav = [
--- src/components/portfolio/WorksItem.jsx
+++ src/components/portfolio/WorksItem.jsx
@@ -5,10 +5,10 @@ const WorksItem = ({ item }) => {
         <div className="work__card" key={item.id}>
             <img src={item.image} alt="" className="work__img" />
             <h3 className="work__title">{item.title}</h3>
-            <a href={`${item.demo}`} className="work__button">
+            <a href={item.link} className="work__button" target="_blank">
                 Demo <i className="bx bx-right-arrow-alt work__button-icon"></i>
             </a>
-            <a href="#" className="work__button">
+            <a href={item.git} className="work__button" target="_blank">
                 GitHub <i className="bx bx-right-arrow-alt work__button-icon"></i>
             </a>
         </div>
